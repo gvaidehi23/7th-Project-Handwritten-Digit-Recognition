1fa3943183659217e55bac2ddfef6fe6b5b605aa
gvaidehi23 <prachigatkal2523@gmail.com>
2026-07-20 23:41:44 +0530

    Update version_check.py

--- version_check.py
+++ version_check.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import tensorflow
-import cv2 
+
 
 print(st.__version__)
 print(pd.__version__)
 print(np.__version__)
 print(tensorflow.__version__)
-print(cv2.__version__)
+
