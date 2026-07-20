40a11e3fd7b7e89af54199cb4fb497bc32b54687
gvaidehi23 <prachigatkal2523@gmail.com>
2026-07-20 23:33:12 +0530

    Add files via upload

---
+++ version_check.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import tensorflow
+
+print(st.__version__)
+print(pd.__version__)
+print(np.__version__)
+print(tensorflow.__version__)
